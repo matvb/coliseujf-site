1779773e1f5a8322e2c914a62ea6963215e27e95
Mateus Villas Boas <mateus.villasboas@gmail.com>
2020-04-09 00:25:30 -0300

    lots of new things

--- gym/js/main.js
+++ gym/js/main.js
@@ -232,11 +232,11 @@ center: false,
     $(this).addClass('active');
     event.preventDefault();
 	});
-  
+
   // wow js
   new WOW().init();
 
-  // counter 
+  // counter
   $('.counter').counterUp({
     delay: 10,
     time: 10000
@@ -411,7 +411,7 @@ $(document).ready(function() {
 
 
 
-//------- Mailchimp js --------//  
+//------- Mailchimp js --------//
 function mailChimp() {
   $('#mc_embed_signup').find('form').ajaxChimp();
 }
@@ -438,13 +438,34 @@ mailChimp();
           $('select').niceSelect();
         });
 
-        // prise slider 
-        
+        // prise slider
 
 
+    /*------------------
+       Timetable Filter
+    --------------------*/
+    $('.timetable-controls ul li').on('click', function() {
+        var tsfilter = $(this).data('tsfilter');
+        $('.timetable-controls ul li').removeClass('active');
+        $(this).addClass('active');
+
+        if(tsfilter == 'all') {
+            $('.classtime-table').removeClass('filtering');
+            $('.ts-item').removeClass('show');
+        } else {
+            $('.classtime-table').addClass('filtering');
+        }
+        $('.ts-item').each(function(){
+            $(this).removeClass('show');
+            if($(this).data('tsmeta') == tsfilter) {
+                $(this).addClass('show');
+            }
+        });
+    });
+
+		/*------------------
+       Timetable Filter End
+    --------------------*/
 
 
-
-
-
-})(jQuery);	
+})(jQuery);
